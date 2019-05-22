d83825d9ca0e971dac9493d9eba6b043b8d58057
Aitor Arjona <aitor.a98@gmail.com>
2019-05-22 08:46:01 +0200

    Added getting conn config from Airflow's gui config

--- hooks/ibm_cf_hook.py
+++ hooks/ibm_cf_hook.py
@@ -5,13 +5,13 @@ import pywren_ibm_cloud
 
 class IbmCloudFunctionsHook(BaseHook):
 
-    def __init__(self):
-        self.exec_active = False
+    def __init__(self, conn_id='ibm_cf_config'):
+        conn = self.get_connection(conn_id)
+        self.config = conn.extra_dejson
 
     def get_conn(self):
         executor = None
-        if (not self.exec_active):
-            executor = pywren_ibm_cloud.ibm_cf_executor()
-            self.exec_active = True
+        if (executor is None):
+            executor = pywren_ibm_cloud.ibm_cf_executor(config=self.config)
         
         return executor
