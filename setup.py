6909c053bd09fe61ecf1a82d2a30bc9252ce8662
Aitor Arjona <aitor.a98@gmail.com>
2020-06-16 17:14:28 +0200

    Added module setup script

--- setup.py
+++ setup.py
@@ -4,17 +4,17 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pywren_ibm_cloud_airflow",
-    version="1.1.1",
+    name="cloudbutton_airflow_plugin",
+    version="1.0.0",
     author="Aitor Arjona",
     author_email="aitor.arjona@urv.com",
-    description="IBM Cloud PyWren plugin for Apache Airflow",
+    description="Cloudbutton plugin for Apache Airflow",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/cloudbutton/ibm-pywren_airflow-plugin",
-    install_requires=[
-        "pywren-ibm-cloud>=1.5.2 "
-    ],
+    url="https://github.com/cloudbutton/airflow-plugin",
+#    install_requires=[
+#        "pywren-ibm-cloud>=1.5.2 "
+#    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
@@ -23,7 +23,7 @@ setuptools.setup(
     python_requires='>=3.6',
     entry_points = {
         'airflow.plugins': [
-            'plugin = pywren_ibm_cloud_airflow.plugin:IBMPyWrenAirflowPlugin'
+            'plugin = cloudbutton_airflow_plugin.plugin:CloudbuttonAirflowPlugin'
         ]
     }
 )
